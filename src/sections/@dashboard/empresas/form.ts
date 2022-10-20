2522cc3e3db2fbf8179647b236a78a2177d00be3
André Rossner <rossnerandre@gmail.com>
2022-10-20 17:37:58 -0300

    alterado estilo dos inputs add input de cpnj alterado layout conforme figma

--- src/sections/@dashboard/empresas/form.ts
+++ src/sections/@dashboard/empresas/form.ts
@@ -4,18 +4,21 @@ import * as Yup from 'yup';
 
 export type FormValuesProps = {
   name: string,
-  contactName: string,
+  contact: string,
   email: string,
+  cnpj: string,
 };
 
 export const defaultValues = {
   name: '',
-  contactName: '',
+  contact: '',
   email: '',
+  cnpj: '',
 };
 
 export const FormSchema = Yup.object().shape({
   name: Yup.string().required('Nome da empresa é obrigatório'),
-  contactName: Yup.string().required('Nome do contato é obrigatório'),
+  contact: Yup.string().required('Nome do contato é obrigatório'),
   email: Yup.string().required('Email da empresa é obrigatório'),
+  cnpj: Yup.string().required('CNPJ da empresa é obrigatório'),
 });
