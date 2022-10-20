428e755f11a8d99e316f22c04fe94c70fa12288f
André Rossner <rossnerandre@gmail.com>
2022-10-20 10:53:23 -0300

    Add botão de Nova empresa, estilizado botão, searchbar

--- src/sections/@dashboard/empresas/EmpresaTableToolbar.tsx
+++ src/sections/@dashboard/empresas/EmpresaTableToolbar.tsx
@@ -1,7 +1,41 @@
 // @mui
-import { Stack, InputAdornment, TextField } from '@mui/material';
+import {Stack, InputAdornment, TextField, Button, Box} from '@mui/material';
 // components
 import Iconify from '../../../components/Iconify';
+import NextLink from "next/link";
+import {styled} from "@mui/material/styles";
+
+const Searchbar = styled(TextField)(({theme}) => ({
+  width: '638px',
+  [theme.breakpoints.down('md')]: {
+    width: '400px'
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '100%'
+  }
+}));
+
+const BoxStyled = styled(Box)(({theme}) => ({
+  [theme.breakpoints.down('sm')]: {
+    marginTop: '1.3rem',
+    width: '100%',
+    '& .MuiButtonBase-root': {
+      width: '100%'
+    }
+  }
+
+}));
+
+const StackStyled = styled(Stack)(({theme}) => ({
+  alignItems: "center",
+  justifyContent: "space-between",
+  flexDirection: 'row',
+  padding: theme.spacing(2.5, 3),
+  [theme.breakpoints.down('sm')]: {
+    flexDirection: 'column',
+  }
+}));
+
 
 // ----------------------------------------------------------------------
 
@@ -10,28 +44,33 @@ type Props = {
   onFilterName: (value: string) => void;
 };
 
-export default function EmpresaTableToolbar({ filterName, onFilterName }: Props) {
+export default function EmpresaTableToolbar({filterName, onFilterName}: Props) {
   return (
-    <Stack
-      direction="row"
-      sx={{ py: 2.5, px: 3 }}
-    >
-      <TextField
-        sx={{ width: '638px' }}
+    <StackStyled>
+      <Searchbar
         value={filterName}
         onChange={(event) => onFilterName(event.target.value)}
-        placeholder="Pesquisar empresa..."
+        placeholder="Pesquisar empresa "
         InputProps={{
           startAdornment: (
             <InputAdornment position="start">
               <Iconify
                 icon={'eva:search-fill'}
-                sx={{ color: 'text.disabled', width: 20, height: 20 }}
+                sx={{color: 'text.disabled', width: 20, height: 20}}
               />
             </InputAdornment>
           ),
         }}
       />
-    </Stack>
+      <BoxStyled>
+        <NextLink href={'/dashboard/empresas/nova'} passHref>
+          <Button
+            variant="contained"
+          >
+            Nova Empresa
+          </Button>
+        </NextLink>
+      </BoxStyled>
+    </StackStyled>
   );
 }
