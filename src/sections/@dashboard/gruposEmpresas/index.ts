9d836342200ee89b3160e5cfb815c4803423f6d5
André Rossner <rossnerandre@gmail.com>
2022-11-04 10:54:08 -0300

    section empresas alterado para grupoEmpresa

--- src/sections/@dashboard/gruposEmpresas/index.ts
+++ src/sections/@dashboard/gruposEmpresas/index.ts
@@ -1,3 +1,4 @@
 export {default as EmpresaTableToolbar} from './EmpresaTableToolbar';
 export {default as EmpresaTableRow} from './EmpresaTableRow';
-export {default as FormEmpresa} from './FormEmpresa';
+// export {default as FormEmpresa} from './FormEmpresa';
+export {default as FormNovoGrupo} from './FormNovoGrupo';
